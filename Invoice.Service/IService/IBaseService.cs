71fbc4ea6edd41115f807c57c8e3ee3fe119c158
Seldin Puce <puceseldin@gmail.com>
2020-11-06 10:30:13 +0100

    Added Invoice.Service data library with BaseService

---
+++ Invoice.Service/IService/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Invoice.Service.IService
+{
+    public interface IBaseService<in TRequest, TResponse, in TKey>
+    {
+        Task<List<TResponse>> GetAll();
+        Task<TResponse> GetById(TKey id);
+        Task<TResponse> Create(TRequest entity);
+        Task<TResponse> Update(TKey id, TRequest entity);
+        Task Delete(TKey id);
+    }
+}
---
+++ Invoice.Service/Service/BaseService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Invoice.Data.Context;
+using Invoice.Service.IService;
+
+namespace Invoice.Service.Service
+{
+    public class BaseService<TEntity, TRequest, TResponse, TKey>
+        : IBaseService<TRequest, TResponse, TKey> where TEntity : class
+    {
+
+        public virtual async Task<List<TResponse>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> GetById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Create(TRequest entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Update(TKey id, TRequest entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
